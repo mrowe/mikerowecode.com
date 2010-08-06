5762e9c6dbc92f5660dc5beef6c85a8fcf67aba7
Michael Rowe <mrowe@mojain.com>
2010-08-06 18:07:37 +1000

    Introduce Disqus comment support.

---
+++ _includes/disqus.js
@@ -0,0 +1,6 @@
+var disqus_shortname = 'mikerowecode';
+(function () {
+  var s = document.createElement('script'); s.async = true;
+  s.src = 'http://disqus.com/forums/mikerowecode/count.js';
+  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());

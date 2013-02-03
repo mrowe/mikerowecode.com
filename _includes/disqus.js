fea493bf5426568e040551dfdb5636a1ca5ddbd7
Michael Rowe <mrowe@mojain.com>
2013-02-04 08:18:58 +1100

    Fix disqus config so "n comments" in byline works

--- _includes/disqus.js
+++ _includes/disqus.js
@@ -1,6 +1,7 @@
 var disqus_shortname = 'mikerowecode';
 (function () {
   var s = document.createElement('script'); s.async = true;
-  s.src = 'http://disqus.com/forums/mikerowecode/count.js';
+  s.type = 'text/javascript';
+  s.src = 'http://mikerowecode.disqus.com/count.js';
   (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
 }());
